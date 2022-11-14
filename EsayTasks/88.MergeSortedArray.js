66f713b746e4a1367900e61bca8dae1e12d2aadc
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-11-14 14:17:36 +0500

    83 added

---
+++ EsayTasks/88.MergeSortedArray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+let merge = function (nums1, m, nums2, n) {
+  while (n) {
+    if (nums1[m - 1] > nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[--m];
+    } else {
+      nums1[m + n - 1] = nums2[--n];
+    }
+  }
+  return nums1;
+};
