4cccd1dcece81a6baf2948bc109ffb433fda932c
Yusupov Jaloliddin <92913529+YusupovJaloliddin@users.noreply.github.com>
2022-11-23 11:46:26 +0500

    Create 1154.DayOfTheYear.js

---
+++ EsayTasks/1154.DayOfTheYear.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+let dayOfYear = function (date) {
+    const year = +date.slice(0, 4);
+    const month = +date.slice(5, 7);
+    const day = +date.slice(8);
+    const d =
+        year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0) ? 29 : 28;
+    const days = [31, d, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let result = day;
+    for (let i = 0; i < month - 1; ++i) {
+        result += days[i];
+    }
+    return result;
+}
