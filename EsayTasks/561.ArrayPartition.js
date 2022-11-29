c26fea2d3a17dad61f0f8d313190d021e9e638bd
Yusupov Jaloliddin <92913529+YusupovJaloliddin@users.noreply.github.com>
2022-11-29 10:48:26 +0500

    Create 561.ArrayPartition.js

---
+++ EsayTasks/561.ArrayPartition.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+let arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let result = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    result += nums[i];
+  }
+  return result;
+};
