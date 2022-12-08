7693d95008ad20ceed4d3e1d4e3280c25f20c6c0
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-12-08 16:54:23 +0500

    solution 1920 updated

--- EsayTasks/1920.BuildArrayFromPermutation.js
+++ EsayTasks/1920.BuildArrayFromPermutation.js
@@ -3,6 +3,10 @@
  * @return {number[]}
  */
 var buildArray = function (nums) {
-  return nums;
+  let newArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    newArr.push(nums[nums[i]]);
+  }
+  return newArr;
 };
-console.log(buildArray([1, 2, 4, 5, 3, 0]));
+console.log(buildArray([5, 0, 1, 2, 3, 4]));
