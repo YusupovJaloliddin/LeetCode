c8045cfb54aa86f8a7928521977ed848a238973d
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-12-16 19:28:10 +0500

    first

---
+++ EsayTasks/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function (seats, students) {
+  return seats + students;
+};
+
+console.log(minMovesToSeat(1, 2));
