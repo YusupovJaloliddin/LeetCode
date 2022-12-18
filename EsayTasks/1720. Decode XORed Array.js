18c47e8b41e168eba86ee6c5f385aca625f0ed6a
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-12-18 23:38:27 +0500

    1720

---
+++ EsayTasks/1720. Decode XORed Array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} encoded
+ * @param {number} first
+ * @return {number[]}
+ */
+var decode = function (encoded, first) {
+  let len = encoded.length;
+  let res = [];
+  res[0] = first;
+  for (let i = 0; i < len; i++) {
+    res[i + 1] = res[i] ^ encoded[i];
+  }
+  return res;
+};
