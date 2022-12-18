c45782dc4b8946eaf4f22e856308cfabb4e3e0f0
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-12-19 01:26:38 +0500

    2373 solution

---
+++ EsayTasks/2373. Largest Local Values in a Matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var largestLocal = function (grid) {
+  // declare (n-2 x n-2) matrix
+  const matrix = new Array(grid.length - 2)
+    .fill(0)
+    .map(() => new Array(grid[0].length - 2).fill(0));
+
+  for (let i = 0; i < grid[i].length - 2; i++) {
+    for (let j = 0; j < grid.length - 2; j++) {
+      //find the max in each 3x3 martix
+      matrix[i][j] = Math.max(
+        grid[i][j],
+        grid[i][j + 1],
+        grid[i][j + 2],
+        grid[i + 1][j],
+        grid[i + 1][j + 1],
+        grid[i + 1][j + 2],
+        grid[i + 2][j],
+        grid[i + 2][j + 1],
+        grid[i + 2][j + 2]
+      );
+    }
+  }
+
+  return matrix;
+};
