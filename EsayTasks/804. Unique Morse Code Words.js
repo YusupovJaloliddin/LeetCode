e58c66fb79dd82d9349f237669c9d3928fb0b801
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2022-12-20 23:42:21 +0500

    804 solved

---
+++ EsayTasks/804. Unique Morse Code Words.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+const uniqueMorseRepresentations = (words) => {
+  const result = [];
+  const codes = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+
+  words.forEach((word) => {
+    let str = "";
+    for (let i = 0; i < word.length; i++) {
+      const code = word[i].charCodeAt(0) - 97;
+      str += codes[code];
+    }
+
+    if (!result.find((v) => v === str)) {
+      result.push(str);
+    }
+  });
+
+  return result.length;
+};
