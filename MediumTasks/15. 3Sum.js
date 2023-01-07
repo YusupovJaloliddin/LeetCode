cdffa5e43d9b49d1dd668cdfae2dee47a5b3f1fb
Yusupov Jaloliddin <yusupovjaloliddin4633@gmail.com>
2023-01-07 21:52:59 +0500

    15. 3Sum

---
+++ MediumTasks/15. 3Sum.js
@@ -0,0 +1,43 @@
+const threeSum = (nums) => {
+  let len = nums.length;
+  if (len < 3) return [];
+  nums.sort(function (a, b) {
+    return a - b;
+  });
+  if (nums[0] > 0 || nums[0] + nums[1] + nums[2] > 0) return [];
+  if (len === 3) {
+    if (nums[0] + nums[1] + nums[2] === 0) return [nums];
+    else return [];
+  }
+  //console.log(nums);
+
+  let result = [];
+  let checker = "";
+
+  for (let i = 0; i < len; i++) {
+    var sum = -nums[i];
+    for (let j = i + 1, k = len - 1; j < k; ) {
+      //console.log(i + ', ' + j + ', ' + k + ': ' + nums[i] + ', ' + nums[j] + ', ' + nums[k]);
+      var temp = nums[j] + nums[k];
+      if (temp < sum) {
+        j++;
+      } else if (temp > sum) {
+        k--;
+      } else {
+        var triplet = [nums[i], nums[j], nums[k]];
+        result.push(triplet);
+
+        //skip j term if it's a duplicate
+        while (nums[j] === nums[j + 1] && j < len) j++;
+        //skip k term if it's a duplicate
+        while (nums[k] === nums[k - 1] && k > 0) k--;
+        j++;
+        k--;
+      }
+    }
+    //skip i term if it's a duplicate
+    while (nums[i] === nums[i + 1] && i < len) i++;
+  }
+
+  return result;
+};
